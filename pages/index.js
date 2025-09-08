f6d06db27d8183428aa27667b77c696534864f7a
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-08 17:36:17 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Head from "next/head";
-import { FaWhatsapp, FaTiktok } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp, FaTiktok, FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   const [url, setUrl] = useState("");

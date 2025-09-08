76a13020f88c55b566148220df2a8271447b7962
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-08 18:35:27 +0800

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,11 @@
+// Simpan banyak cookies di sini
+// Copy dari DevTools -> Application -> Cookies -> instagram.com
+// Format harus sama persis kayak di browser
+
+const cookies = [
+  "sessionid=12345; csrftoken=abcdef; ds_user_id=99999;",
+  "sessionid=67890; csrftoken=ghijkl; ds_user_id=88888;",
+  // Bisa tambah banyak cookies lagi
+];
+
+module.exports = cookies;
